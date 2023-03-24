4c332b7aa9e997efe7b46a7a42deac604d8f88a9
Hrvoje Ramljak <hrwamo@gmail.com>
2023-03-24 15:00:23 +0100

    radimo prvi commit na projektu u cilju pusha na repozitorij

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  return (
+    <div>
+      <div>
+        <Card className="expenses">
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+          />
+          {props.items.map((expense) => {
+            return (<ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />)
+            }
+           )
+          }
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default Expenses;
